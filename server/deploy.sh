a95c66b51ff8b4ceeb29e98a8d3656cb77ab18f9
Rich W <richard.wilson@smgdesign.org>
2022-06-01 17:49:52 +0100

    server model finished

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 VERSION=v1.3.1.0
-docker build -t x-tunnel:${VERSION} /opt/x-tunnel/source
-
-docker cp smgdesign/x-tunnel:${VERSION}:/app.tar.gz /opt/x-tunnel/assets
-tar -zxvf /opt/x-tunnel/assets/app.tar.gz -O /opt/x-tunnel
-yarn start --host x-tunnel.x-smg.com --port 80
+TGT=/opt/x-tunnel
+SSH=root@rack04.vpn.smg.systems
+docker build -t smgdesign/x-tunnel:${VERSION} .
+docker create -it --name x-tunnel smgdesign/x-tunnel:${VERSION} bash
+docker cp x-tunnel:/x-tunnel.tar.gz ./app.tar.gz
+docker rm -f x-tunnel
+scp ./app.tar.gz ${SSH}:${TGT}
+rm -rf ./app.tar.gz
+ssh ${SSH} << EOF
+mkdir ${TGT}/${VERSION}
+tar -zxvf ${TGT}/app.tar.gz -C ${TGT}/${VERSION}
+rm -rf ${TGT}/app.tar.gz
+pm2 stop all
+rm ${TGT}/app
+ln -s ${TGT}/${VERSION}/app ${TGT}/app
+cd ${TGT}/app
+yarn start --domain x-tunnel.x-smg.com --port 80
+EOF
